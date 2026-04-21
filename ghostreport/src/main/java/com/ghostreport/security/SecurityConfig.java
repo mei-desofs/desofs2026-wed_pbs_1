4b6988b36ac03991651097a40fcafeb3fad9206a
sofissg13 <1250559@isep.ipp.pt>
2026-04-21 19:36:54 +0100

    testes

--- ghostreport/src/main/java/com/ghostreport/security/SecurityConfig.java
+++ ghostreport/src/main/java/com/ghostreport/security/SecurityConfig.java
@@ -2,14 +2,11 @@ package com.ghostreport.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -19,33 +16,16 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/reports").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/reports/*/verify").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/reports/*/attachments").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
-
+                        .requestMatchers("/reports", "/reports/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/analyst/**").hasAnyRole("ANALYST", "ADMIN")
-
-                        .anyRequest().authenticated()
+                        // testes, depois deve-se tirar
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder
-    ) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
