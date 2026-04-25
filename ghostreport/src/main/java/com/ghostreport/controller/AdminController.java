8a9af163d8860a8a00edc2750fc7a36ab6fba356
barbarasilva9 <1250497@isep.ipp.pt>
2026-04-25 18:25:27 +0100

    Admin html update auditlogs e security logs

--- ghostreport/src/main/java/com/ghostreport/controller/AdminController.java
+++ ghostreport/src/main/java/com/ghostreport/controller/AdminController.java
@@ -2,12 +2,14 @@ package com.ghostreport.controller;
 
 import com.ghostreport.dto.CreateUserRequest;
 import com.ghostreport.dto.UserResponse;
+import com.ghostreport.repository.AuditLogRepository;
 import com.ghostreport.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.ghostreport.model.SecurityAlert;
 import com.ghostreport.repository.SecurityAlertRepository;
+import com.ghostreport.model.AuditLog;
 import java.util.List;
 
 import java.util.List;
@@ -18,10 +20,12 @@ public class AdminController {
 
     private final UserService userService;
     private final SecurityAlertRepository securityAlertRepository;
+    private final AuditLogRepository auditLogRepository;
 
-    public AdminController(UserService userService, SecurityAlertRepository securityAlertRepository) {
+    public AdminController(UserService userService, SecurityAlertRepository securityAlertRepository, AuditLogRepository auditLogRepository) {
         this.userService = userService;
         this.securityAlertRepository = securityAlertRepository;
+        this.auditLogRepository = auditLogRepository;
     }
 
     @GetMapping("/panel")
@@ -32,6 +36,11 @@ public class AdminController {
     @GetMapping("/users")
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/audit-logs")
+    public List<AuditLog> getAuditLogs() {
+        return auditLogRepository.findAll();
     }
 
     @GetMapping("/security-alerts")
